c1fc93e979c50cd720c0864e4c572d8c516f4368
Andrew Volosevich <volosevich1989@gmail.com>
2021-03-11 11:44:54 +0300

    add colors

--- assigment/client/src/components/cell/index.tsx
+++ assigment/client/src/components/cell/index.tsx
@@ -3,39 +3,39 @@ import classNames from 'classnames';
 import { useSelector } from 'react-redux';
 import {selectSize} from "../../store/features/game/gameSlice";
 import styles from "./cell.module.scss";
+import {ReactComponent as CellImg} from '../../assets/cell.svg';
 
 const Cell = (props: {
   x: number,
   y: number,
   z: number,
-  value: number
+  value: number,
+  id: string,
 }) => {
   const cellRef = useRef(null)
   const size = useSelector(selectSize)
 
-  const cellClasses = classNames(styles.element, {
+  const cellClasses = classNames(styles.element, 'cell', {
     [styles.size3]: size === 3,
     [styles.size4]: size === 4,
+    [styles[`color-${props.value}`]]: props.value
   })
   return (
-   <>
-     <div
-       ref={cellRef}
-       className={cellClasses}
-       data-value={props.value}
-       data-x={props.x}
-       data-y={props.y}
-       data-z={props.z}
-     >
-       <span>{props.value === 0 ? ' ' : props.value}</span>
-     </div>
-   </>
+    <>
+      <div
+        ref={cellRef}
+        className={cellClasses}
+        id={props.id}
+        data-value={props.value}
+        data-x={props.x}
+        data-y={props.y}
+        data-z={props.z}
+      >
+        <CellImg />
+        <span>{props.value === 0 ? ' ' : props.value}</span>
+      </div>
+    </>
   );
 }
 
 export default Cell;
-
-
-
-
-
